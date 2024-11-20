035a76a694b57de22f3f1e4f9f4658ec8c4c7524
Felix Notka <felix.notka@cyan-it.de>
2024-11-20 12:41:27 +0100

    Update DNS Scripts

--- .kubernetes/createDNSentry.sh
+++ .kubernetes/createDNSentry.sh
@@ -10,15 +10,15 @@ mv ~/doctl /usr/local/bin
 doctl auth init -t #{DOCTL_AUTH_KEY}
 
 ### make sure domain does not already exist, then create it
-if [[ ! $(doctl compute domain records list #{BASE_DOMAIN} | grep -i #{Octopus.Action[GetFN].Output.FeatureName}-portal) ]]
+if [[ ! $(doctl compute domain records list #{BASE_DOMAIN} | grep -i obs-#{Octopus.Action[GetFN].Output.FeatureName}) ]]
 then
 
     echo "---> Now creating subdomain $FeatureName for #{BASE_DOMAIN} ..."
 
     doctl compute domain records create #{BASE_DOMAIN} \
         --record-type A \
-        --record-name #{Octopus.Action[GetFN].Output.FeatureName}-portal \
-        --record-data #{IP_INGRESS_CONTROLER}
+        --record-name obs-#{Octopus.Action[GetFN].Output.FeatureName} \
+        --record-data #{IP_INGRESS_CONTROLLER}
 
 else
     echo "---> DNS entry already exists, doing nothing ..."
--- .kubernetes/removeFeature.sh
+++ .kubernetes/removeFeature.sh
@@ -6,48 +6,37 @@
 ### -> delete complete namespace with that feature name
 ### -> delete feature db
 
-cd #{PaketName}
-isFeature=$(cat ./gitlab-commit.env | sed ' s/-//g ' | grep "Merge branch 'feature/[[:alnum:]]\+' into")
+### install doctl
+cd ~
+wget #{WGET_PARAM} https://github.com/digitalocean/doctl/releases/download/v1.71.0/doctl-1.71.0-linux-amd64.tar.gz
+tar xf ~/doctl-1.71.0-linux-amd64.tar.gz
+mv ~/doctl /usr/local/bin
 
-if [[ $isFeature ]]
-then
-   ### install doctl
-   cd ~
-   wget #{WGET_PARAM} https://github.com/digitalocean/doctl/releases/download/v1.71.0/doctl-1.71.0-linux-amd64.tar.gz
-   tar xf ~/doctl-1.71.0-linux-amd64.tar.gz
-   mv ~/doctl /usr/local/bin
+### authenticate
+doctl auth init -t #{DOCTL_AUTH_KEY}
 
-   ### authenticate
-   doctl auth init -t #{DOCTL_AUTH_KEY}
+FeatureNumber=$(echo $isFeature | grep -o -E feature/[[:alnum:]]\+ |  sed ' s/[a-zA-Z\/]//g ')
+FeatureName=$(echo "feature$FeatureNumber")
+FeatureNameDNS=$(echo "obs-$FeatureName")
+DNSRecordId=$(doctl compute domain records list #{BASE_DOMAIN} | grep $FeatureNameDNS | awk ' { print $1 } ')
+FeatureNamespace=$(kubectl get namespaces | grep "observability-demo-$FeatureName")
 
-  FeatureNumber=$(echo $isFeature | grep -o -E feature/[[:alnum:]]\+ |  sed ' s/[a-zA-Z\/]//g ')
-  FeatureName=$(echo "feature$FeatureNumber")
-  FeatureNameDNS=$(echo "$FeatureName-portal")
-  DNSRecordId=$(doctl compute domain records list #{BASE_DOMAIN} | grep $FeatureNameDNS | awk ' { print $1 } ')
-  FeatureNamespace=$(kubectl get namespaces | grep "cp-$FeatureName")
+echo "---> Now deleting subdomain if necessary ..."
 
-  echo "---> Now deleting subdomain if necessary ..."
-
-  if [[ $DNSRecordId ]]
+if [[ $DNSRecordId ]]
   then
     echo "---> Deleting subdomain ID $DNSRecordId / $FeatureNameDNS from #{BASE_DOMAIN} ..."
     doctl compute domain records delete #{BASE_DOMAIN} $DNSRecordId -f
   else
     echo "---> Subdomain $FeatureNameDNS does not exist, nothing to delete ..."
-  fi
+fi
 
-  echo "---> Now deleting namespace if necessary ..."
+echo "---> Now deleting namespace if necessary ..."
 
-  if [[ $FeatureNamespace ]]
+if [[ $FeatureNamespace ]]
   then
-    echo "---> Deleting namespace cp-$FeatureName ..."
-    kubectl delete namespace cp-$FeatureName
+    echo "---> Deleting namespace observability-demo-$FeatureName ..."
+    kubectl delete namespace observability-demo-$FeatureName
   else
-    echo "---> Namespace cp-$FeatureName does not exist, nothing to delete ..."
-  fi
-
-  echo "---> Now deleting database if necessary ..."
-  kubectl exec -n cp-feature-db -c cp-feature-db db-0 -- /opt/mssql/featuredb/DBdrop_feature.sh $FeatureName
-else
-  echo "---> Not a feature merge, doing nothing ..."
+    echo "---> Namespace observability-demo-$FeatureName does not exist, nothing to delete ..."
 fi
